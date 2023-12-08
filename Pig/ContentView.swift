f1868ba701674af15fcf1bc645b4f33b7419dbc9
Bennett Stern <bstern@bsd220.org>
2023-12-08 09:53:10 -0600

    Fixed game score

--- Pig/ContentView.swift
+++ Pig/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 struct ContentView: View {
     @State private var turnScore = 0
-    @State private var gameScore = 90
+    @State private var gameScore = 0
     @State private var randomValue = 0
     @State private var rotation = 0.0
     @State private var gameOver = false
