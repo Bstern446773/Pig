937996971d715e6139a60420a4de4241dff53847
Bennett Stern <bstern@bsd220.org>
2023-12-08 09:48:42 -0600

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Bennett Stern on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
